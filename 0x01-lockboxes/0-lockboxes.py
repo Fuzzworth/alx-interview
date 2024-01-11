43ee9ee96fb0678a23ce4bed19d537345b174cb9
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-11 23:09:42 +0200

    First try

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,4 +8,13 @@ def canUnlockAll(boxes):
     '''
     Function Docs
     '''
-    
+    if (boxes):
+        numOfBoxes = len(boxes)
+        setOfKeys = {0}
+        for box in boxes:
+            for key in box:
+                result = setOfKeys.add(key)
+        for n in range(numOfBoxes):
+            if n not in setOfKeys:
+                return False
+        return True
