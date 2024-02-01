4838976bd84baaa69fc15c79bce2628ab6b04eaa
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-02-01 20:41:28 +0200

    Task 0 Fix: 2

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -20,7 +20,7 @@ def validUTF8(data: List[int]) -> bool:
     # Iterate through each byte in the data
     i = 0
     while i < len(data):
-        # Count the number of consecutive set bits starting from the 
+        # Count the number of consecutive set bits starting from the
         # most significant bit
         num_bytes = 0
         mask = 1 << 7
