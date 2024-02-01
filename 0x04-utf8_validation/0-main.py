0c39e7a3232d7cbf87fbf53eda30c6eaf70d5a23
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-02-01 20:36:31 +0200

    Task 0

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -1,40 +1,15 @@
 #!/usr/bin/python3
 """
-Module Docs
+Main file for testing
 """
 
+validUTF8 = __import__('0-validate_utf8').validUTF8
 
-def validUTF8(data):
-    """
-    Determine if a given data set represents a valid UTF-8 encoding.
+data = [65]
+print(validUTF8(data))
 
-    Args:
-    - data (list of int): List of integers representing 1-byte data.
-    Each integer represents 8 least significant bits.
+data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data))
 
-    Returns:
-    - bool: True if data is a valid UTF-8 encoding, else return False.
-    """
-
-    # Iterate through each byte in the data
-    i = 0
-    while i < len(data):
-        # Count the number of consecutive set bits starting from the most significant bit
-        num_bytes = 0
-        mask = 1 << 7
-        while mask & data[i]:
-            num_bytes += 1
-            mask >>= 1
-
-        # Validate the number of bytes based on the rules for UTF-8 encoding
-        if num_bytes == 1 or num_bytes > 4:
-            return False
-
-        # Check that the following bytes are valid continuations
-        for j in range(1, num_bytes):
-            if i + j >= len(data) or (data[i + j] >> 6) != 0b10:
-                return False
-
-        i += num_bytes
-
-    return True
+data = [229, 65, 127, 256]
+print(validUTF8(data))
---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Module Docs
+"""
+
+
+def validUTF8(data):
+    """
+    Determine if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+    - data (list of int): List of integers representing 1-byte data.
+    Each integer represents 8 least significant bits.
+
+    Returns:
+    - bool: True if data is a valid UTF-8 encoding, else return False.
+    """
+
+    # Iterate through each byte in the data
+    i = 0
+    while i < len(data):
+        # Count the number of consecutive set bits starting from the most significant bit
+        num_bytes = 0
+        mask = 1 << 7
+        while mask & data[i]:
+            num_bytes += 1
+            mask >>= 1
+
+        # Validate the number of bytes based on the rules for UTF-8 encoding
+        if num_bytes == 1 or num_bytes > 4:
+            return False
+
+        # Check that the following bytes are valid continuations
+        for j in range(1, num_bytes):
+            if i + j >= len(data) or (data[i + j] >> 6) != 0b10:
+                return False
+
+        i += num_bytes
+
+    return True
